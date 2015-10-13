19644319f87e76d4189ffffcab52edf062373535
Edmund Li <edmund.xz.lee@gmail.com>
2015-10-13 10:05:50 -0700

    fetch all albums done

--- app/assets/javascripts/components/home/sidebar/all_albums_btn.js.jsx
+++ app/assets/javascripts/components/home/sidebar/all_albums_btn.js.jsx
@@ -1,12 +1,13 @@
 var AllAlbumsBtn = React.createClass({
 	render: function () {
 		return (
-			<div className="sidebar-thumbs" onClick={this.onClickHandler}></div>
+			<div className="sidebar-thumbs" onClick={this.onClickHandler}>Album</div>
 		);
 	},
 
 	onClickHandler: function () {
 		// fetech all albums
 		// call ApiUtil.fetchAllAlbums
+		ApiUtil.fetchAllAlbums();
 	}
 });
--- app/assets/javascripts/util/api_util.js
+++ app/assets/javascripts/util/api_util.js
@@ -4,6 +4,16 @@ window.ApiUtil = {
 	},
 
 	fetchAllAlbums: function () {
+		// get request
 		// dispatch action
+		$.ajax({
+			url: '/api/albums',
+			type: 'get',
+			dataType: 'json',
+
+			success: function (respData) {
+				ApiActions.receiveAllAlbums(respData);
+			}
+		});
 	}
 };
