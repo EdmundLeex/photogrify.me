04f9d2e622a753a4b6b68d4dbe365302a0704ef8
Edmund Li <edmund.xz.lee@gmail.com>
2015-10-12 20:24:06 -0700

    added albums api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root "static_pages#root"
 
-  post "signup", to: "users#create"
-  post "signin", to: "sessions#create"
-  post "logout", to: "sessions#destroy"
+  post   "signup", to: "users#create"
+  post   "signin", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  namespace :api do
+  	resources :albums
+  end
 end
