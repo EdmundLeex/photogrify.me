aeab7046a2d147346b177242bc351a679d028855
Edmund Li <edmund.xz.lee@gmail.com>
2015-10-13 14:13:38 -0700

    fixed css layout

--- app/assets/javascripts/components/home/main/album_show/album_show.js.jsx
+++ app/assets/javascripts/components/home/main/album_show/album_show.js.jsx
@@ -1,7 +1,7 @@
 var AlbumShow = React.createClass({
 	render: function () {
 		return (
-			<div className="album_show">
+			<div className="album-show">
 				<AlbumShowTitle />
 				<PicturesCollection />
 			</div>
