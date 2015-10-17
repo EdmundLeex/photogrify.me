276927102819e1a741652cfe305a09a43440d778
Edmund Li <edmund.xz.lee@gmail.com>
2015-10-17 12:20:12 -0700

    style

--- app/assets/javascripts/components/sidebar/all_albums_btn.js.jsx
+++ app/assets/javascripts/components/sidebar/all_albums_btn.js.jsx
@@ -1,7 +1,9 @@
 var AllAlbumsBtn = React.createClass({
 	render: function () {
 		return (
-			<div className="sidebar-thumbs" onClick={this.onClickHandler}>Album</div>
+			<div className="sidebar-thumbs" onClick={this.onClickHandler}>
+				<span className="glyphicon glyphicon-book"></span>
+			</div>
 		);
 	},
 
--- app/assets/javascripts/components/sidebar/all_pictures_btn.js.jsx
+++ app/assets/javascripts/components/sidebar/all_pictures_btn.js.jsx
@@ -1,7 +1,7 @@
 var AllPicturesBtn = React.createClass({
 	render: function () {
 		return (
-			<div className="sidebar-thumbs" onClick={this.onClickHandler}></div>
+			<div className="sidebar-thumbs" onClick={this.onClickHandler}>Pictures</div>
 		);
 	},
 
