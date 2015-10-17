8ebf73d3443d09bd5db716993d6cf3c55f436fd2
Edmund Li <edmund.xz.lee@gmail.com>
2015-10-17 13:44:57 -0700

    style

--- app/assets/javascripts/components/sidebar/all_pictures_btn.js.jsx
+++ app/assets/javascripts/components/sidebar/all_pictures_btn.js.jsx
@@ -1,7 +1,9 @@
 var AllPicturesBtn = React.createClass({
 	render: function () {
 		return (
-			<div className="sidebar-thumbs" onClick={this.onClickHandler}>Pictures</div>
+			<div className="sidebar-thumbs" onClick={this.onClickHandler}>
+				<span className="glyphicon glyphicon-th"></span>
+			</div>
 		);
 	},
 
