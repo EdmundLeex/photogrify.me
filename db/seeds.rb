76d121b4c32aeedfeb41d5e14b023a3ed550c31d
Edmund Li <edmund.xz.lee@gmail.com>
2015-10-19 13:30:06 -0700

    formated seeding progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,29 +18,41 @@ rescue
 end
 
 seed_imgs = []
-failed_imgs = []
-img_file = File.readlines('./db/images/images.txt')
+failed_imgs = {}
+img_file = File.read('./db/images/images.txt').split("\n")
+
+puts "Uploading to Cloudinary:"
 img_file.each do |url|
 	begin
 		seed_imgs << Cloudinary::Uploader.upload(url, cloud_name: ENV['cloud_name'],
 																									public_id: ENV['public_id'],
 																									api_key: ENV['api_key'],
 																									api_secret: ENV['api_secret'])
-	rescue CloudinaryException
-		failed_imgs << url
+	rescue CloudinaryException => e
+		failed_imgs[url] = e
 		next
 	end
+	print "."
 end
+print "\n"
+puts "Upload finished."
 
+puts "Persisting Cloudinary urls to database"
 seed_imgs.each do |img|
 	Album.first.pictures.create(picture_url: img['url'], public_id: img['public_id'])
+	print "."
 end
+print "\n"
 
 Album.first.update(cover_picture_url: seed_imgs[0]['url'])
 
+
 puts "=" * 40
-puts "#{failed_imgs.size} failed to be uploaded."
+puts "#{failed_imgs.size} file(s) failed to be uploaded."
 puts "-" * 40
 puts "Upload failed images:"
-failed_imgs.each { |img| puts "- " + img }
+failed_imgs.each do |url, error|
+	puts "- " + url
+	puts "  " + error.to_s
+end
 puts "=" * 40
